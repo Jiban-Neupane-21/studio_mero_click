fc1d283fcce01d81db9d4e94855e9b66a8902f5b
Jiban-Neupane-21 <neuzbadxhah@gmail.com>
2026-07-31 21:04:04 +0545

    fix dist folder for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import legacy from "@vitejs/plugin-legacy"; // <-- YO ADD GARNUHOS
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./", // <-- YO LINE YAHA (plugins bhanda bahira) RAKHNUHOS
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    legacy({
+      targets: ["defaults", "not IE 11"], // Pure traditional JavaScript convert garne
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 3000,
