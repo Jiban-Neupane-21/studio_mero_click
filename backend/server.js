a902a27d4151bfa3612b85467162cc23000016b2
Jiban-Neupane-21 <neuzbadxhah@gmail.com>
2026-08-07 22:48:51 +0545

    edit server

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,7 @@ app.use((req, res) => {
 });
 
 // --- 5. START SERVER (only once, only here) ---
-const PORT =
-  process.env.PORT || (process.env.NODE_ENV === "production" ? 3000 : 3001);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server successfully listening on port ${PORT}`);
