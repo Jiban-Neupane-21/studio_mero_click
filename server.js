0d74b76632c0d09f4286eeb02d646f80862ffd20
Jiban-Neupane-21 <neuzbadxhah@gmail.com>
2026-08-07 08:01:03 +0545

    test passenger

--- server.js
+++ server.js
@@ -2,20 +2,30 @@ import express from "express";
 
 const app = express();
 
-console.log("PORT =", process.env.PORT);
-
-const PORT = process.env.PORT || 3001;
+console.log("=================================");
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("PORT:", process.env.PORT);
+console.log("=================================");
 
 app.get("/", (req, res) => {
-  res.send("Working");
+    console.log("GET /");
+    res.send("Application is working.");
 });
 
 app.get("/health", (req, res) => {
-  res.json({
-    status: "OK",
-  });
+    console.log("GET /health");
+    res.json({
+        status: "OK",
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+app.use((req, res) => {
+    console.log("404:", req.url);
+    res.status(404).send("Not found");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Listening on ${PORT}`);
 });
