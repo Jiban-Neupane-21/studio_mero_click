71da32c16f87107505ae3f6e1b3f3a7931775b7a
Jiban-Neupane-21 <neuzbadxhah@gmail.com>
2026-08-07 08:54:47 +0545

    test passenger

--- server.js
+++ server.js
@@ -8,24 +8,23 @@ console.log("PORT:", process.env.PORT);
 console.log("=================================");
 
 app.get("/", (req, res) => {
-    console.log("GET /");
-    res.send("Application is working.");
+  console.log("GET /");
+  res.send("Application is working.");
 });
 
 app.get("/health", (req, res) => {
-    console.log("GET /health");
-    res.json({
-        status: "OK",
-    });
+  console.log("GET /health");
+  res.json({
+    status: "OK",
+  });
 });
 
 app.use((req, res) => {
-    console.log("404:", req.url);
-    res.status(404).send("Not found");
+  console.log("404:", req.url);
+  res.status(404).send("Not found");
 });
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Listening on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
